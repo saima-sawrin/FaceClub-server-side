3c8e99bdbd58151d124a25d324429b0bdf80e280
Saima <saimasawrin324@gmail.com>
2022-12-30 13:15:27 +0600

    Get data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { MongoClient, ServerApiVersion , ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion  } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -11,13 +11,34 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/allServices', async (req, res) => {
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.9wy3smt.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+  try{
+    const postCollection = client.db('faceClub').collection('Post');
     
-    const query = {}
-    const cursor = serviceCollection.find(query).sort({price:1});
-    const services = await cursor.toArray();
-    res.send(services);
-    });
+
+    app.get('/Post', async (req, res) => {
+      const query = {}
+      const cursor = postCollection.find(query).sort({like:1});
+      const services = await cursor.limit(3).toArray();
+      res.send(services);
+      });
+    app.get('/allPost', async (req, res) => {
+      const query = {}
+      const cursor = postCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+      });
+  }
+  finally{
+
+  }
+}
+run().catch(err => console.error(err));
+
+
 app.get('/', (req, res) => {
     res.send('Face Club server is running')
     })
